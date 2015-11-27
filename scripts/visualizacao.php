b7f4fefa8be0cc3d4b059bd5a0f56548fa753e3d
Renato Fabbri <renato.fabbri@gmail.com>
2015-11-27 12:41:21 -0200

    hack minimo estah feito p visualizacao com o d3

--- scripts/visualizacao.php
+++ scripts/visualizacao.php
@@ -1,17 +1,87 @@
 <head>
 <script src="js/jquery.js"></script>
+<script src="js/underscore-min.js"></script>
+<script src="js/d3.min.js"></script>
+<style>
+.mcentered {
+    margin: 0 auto;
+    text-align: center;
+}
+</style>
+
 </head>
 
 <script>
+function mNormalize(dataset){
+ds=[];
+dataset.forEach(function(md){
+	limit=Math.pow(10,md.toString().length);
+	md_=(md/limit)*hmax;
+	ds.push(md_)
+});
+return ds;
+}
+function tDraw(mdata){
+	// primeiro: pegamos numero de ips, de problemas, de solucoes e de votos
+	ips=[]
+	nprobs= 0;
+	nsols = 0;
+	nvots = 0;
+	mdata.forEach(function(md){
+		//console.log(md.ip);
+		ips.push(md.ip);
+		if (md.metodo==1){
+			nprobs+=1;
+		} else if (md.metodo==3) {
+			nsols+=1;
+		} else {
+			nvots+=1;
+		}
+	// fazemos um plot como grafico de barra
+
+})
+ips_=_.unique(ips);
+nips=ips_.length;
+
+dataset=[nips,nprobs,nsols,nvots]
+dataset_=mNormalize(dataset); // para agilizar a escala 
+svg.selectAll("rect")
+   .data(dataset_)
+   .enter()
+   .append("rect")
+   .attr("x", function(d,i){return i*40;})
+   .attr("y", function(d){return h-d;})
+   .attr("width", 20)
+   .attr("height", function(d){return d;});
+svg.selectAll("text")
+   .data(dataset_)
+   .enter()
+   .append("text")
+   .attr("x", function(d,i){return i*40;})
+   .attr("y", function(d){return h-d-10;})
+   .text(function(d,i){return dataset[i];});
+
+// colocamos numeros originais na parte de cima do svg
+// embelezamos a parte gráfica
+// fazemos o fundo com as transacoes no tempo
+}
 $(document).ready(function(){
 
 function doStuff(){
   //do some things
 	  setInterval(continueExecution, 3000);
 }
+w=500;
+h=200;
+hmax=h*0.8
+svg = d3.select(".mcentered")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
 
 function continueExecution() {
-	console.log("esso");
+//	console.log("esso");
 	// refazer a query php,
 	// acho que chamando um arquivo externo
 
@@ -21,13 +91,13 @@ function continueExecution() {
         })
         .done(function(resultt) {
 		mdata=resultt;
+		tDraw(mdata);
         });
 	
 }
 continueExecution();
 doStuff();
+// varre mdata para fazer a visualização
 });
-// dar um pulso periódico
-// depois disso fazer a query no servidor e devolver periodicamente
 </script>
-BANANA!
+<div class="mcentered"></div>
